3912bac844e17de8f728961b9c058b7f14f16319
Sam Lelouey <123483340+samlelouey@users.noreply.github.com>
2023-01-25 19:59:14 +0100

    Update damier.py

--- damier.py
+++ damier.py
@@ -1,19 +1,33 @@
 import pygame
 
+#largeur et hauteur de l'image
 WIDTH, HEIGHT = 800, 800
+
+#nb de lignes et de colonnes
 LINES, COLS = 10, 10
+
+#taille d'une case
 SQUARE_SIZE = WIDTH//COLS
 
-CASE_CLAIRE = (220,191,145)
+#couleurs des 2 types de cases
+CASE_CLAIRE = (220,191,145)
 CASE_SOMBRE = (138,88,41)
 
+#classe pour le damier
 class Damier:
     
+    #fonction qui permet de dessiner le damier
     def draw_cel(self, display):
-        display.fill(CASE_SOMBRE) #
+        #créé une image de couleur "CASE_SOMBRE"
+        display.fill(CASE_SOMBRE)
+        
+        #boucles permettant de dessiner les cases de couleur "CASE_CLAIRE"
         for line in range(LINES):
+            #intervalle partant de "line % 2" jusqu'à "COLS" avec un pas de 2
             for col in range(line % 2, COLS, 2):
-                pygame.draw.rect(display, CASE_CLAIRE, (line*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)) #
+                #(line*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE) = les 4 coordonnées du carré représentant une case
+                pygame.draw.rect(display, CASE_CLAIRE, (line*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+ #
 
 
 # CLASSE PRINCIPALE ------------------------------------------------------------
