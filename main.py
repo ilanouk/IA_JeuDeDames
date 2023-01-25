26aedd525be239ff59b099bcdfca67b2b157e96c
Ilan <106468200+ilanouk@users.noreply.github.com>
2023-01-25 19:23:18 +0100

    ajout

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
 import pygame, sys
 from pygame.locals import *
 
-WINDOWWIDTH=1300
-WINDOWHEIGHT=1300
-CELLSIZE=50
-assert WINDOWWIDTH % CELLSIZE==0, "la largeur de la fenetre doit etre un multiple de la taille de la cellule"
-assert WINDOWHEIGHT % CELLSIZE==0, "la hauteur de la fenetre doit etre un multiple de la taille de la cellule"
+WINDOWWIDTH=1100 #largeur fenetre
+WINDOWHEIGHT=1100 #hauteur fenetre
+SPACESIZE=50 #taille pour chaque espace
+BOARDWIDTH=10 #nb de colonnes
+BOARDHEIGHT=10 #nb de lignes
+EMPTY_SPACE='EMPTY_SPACE'
 
-#couleur
+#couleurs
 BLANC = (255,255,255)
 NOIR  = (0,0,0)
 GRIS  = (155,155,155)
 
 #fonction principale qui permet de lancer le programme
 def main():
-    pygame.init()
-    DISPLAYSURF = pygame.display.set_mode(WINDOWWIDTH,WINDOWHEIGHT)
-    BASICFONT = pygame.font.Font(GRILLE)
-    pygame.display.set_caption('Jeu de dames')
+    global MAINCLOCK, DISPLAYSURF, FONT, BIGFONT
 
-    affichage()
+    pygame.init()
+
+    MAINCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
+    pygame.display.set_caption('Jeu de dames')
+    FONT = pygame.font.Font('freesansbold.ttf', 16)
+    BIGFONT = pygame.font.Font('freesansbold.ttf', 32)
+
+    boardImage = pygame.image.load('nomImageDamier.png')###########peut etre ajouter un damier, pas sur
+    #redimensionner l'image
+    boardImage = pygame.transform.smoothscale(boardImage, (BOARDWIDTH * SPACESIZE, BOARDHEIGHT * SPACESIZE))
+
     while True:
         runGame()
 
+def runGame():
+    mainBoard = getNewBoard()
+    resetBoard(mainBoard)
+    drawBoard(mainBoard)
 
-def affichage():
-    titre=pygame.font.Font()
+def getNewBoard():
+    board = []
+    for i in range(BOARDWIDTH):
+        board.append([EMPTY_SPACE]*BOARDHEIGHT)
+    
+    return board
+
+def resetBoard(board): #A FAIRE : METTRE UNE CASE SUR 2 EN BLANC PUIS EN GRIS###############
+    #crée le tableau
+    for x in range(BOARDWIDTH):
+        for y in range(BOARDHEIGHT):
+            board[x][y]=EMPTY_SPACE
+    
+    #A FAIRE : AJOUTER LES PIONS ############
 
 
 def score():
